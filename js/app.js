4f130bb50c87368bee09e554ecf155259db80f2e
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-01-21 23:19:25 +0500

    update all

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// function read_and_show(url) {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
+//     xhr.send();
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//             let data = JSON.parse(xhr.responseText);
+//             console.log(data);
+//         }
+//     };
+// }
+// read_and_show(url);
