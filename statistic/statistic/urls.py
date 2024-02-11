6711d5ec7179adc69620bae7f6b92e2147f0fa8e
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-02-11 20:14:20 +0500

    update

--- statistic/statistic/urls.py
+++ statistic/statistic/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("grappelli/", include("grappelli.urls")),
+    # path("grappelli/", include("grappelli.urls")),
     path("admin/", admin.site.urls),
     path("", include("main_app.urls", namespace="main_app")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
