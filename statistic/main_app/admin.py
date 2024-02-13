1019c66ea016b8554c797e4fcc190756271895a1
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-02-13 14:29:02 +0500

    update

--- statistic/main_app/admin.py
+++ statistic/main_app/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models 
 
 admin.site.register(models.Habits_category)
+admin.site.register(models.Daily_Works)
--- statistic/main_app/models.py
+++ statistic/main_app/models.py
@@ -1,7 +1,29 @@
 from django.db import models
+from datetime import timedelta
+
 
 class Habits_category(models.Model):
     category_name = models.CharField(max_length=150)
-    
-    def __str__ (self):
+
+    def __str__(self):
         return str(self.category_name)
+
+
+from django.db import models
+
+
+class Daily_Works(models.Model):
+    name = models.CharField(max_length=150)
+    day_work = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    works_category = models.ForeignKey(
+        Habits_category,
+        on_delete=models.CASCADE,
+        related_name="daily_works",
+    )
+    workduration = models.DurationField(default=timedelta())
+
+    def save(self, *args, **kwargs):
+        self.workduration = self.end_time - self.start_time
+        super(Daily_Works, self).save(*args, **kwargs)
